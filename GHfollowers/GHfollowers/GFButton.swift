3fb61a48827b5cf18a6aa77eca683bc3964b98f0
Yazan Ghunaim <yazanghunaim07@gmail.com>
2024-01-29 08:55:31 +0100

    Custom UIButton and UITextField

---
+++ GHfollowers/GHfollowers/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GHfollowers
+//
+//  Created by Yazan Ghunaim on 1/29/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    // Used when initialized using storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline) // Dynamic Typography
+        // use auto layout
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ GHfollowers/GHfollowers/GFTextField.swift
@@ -0,0 +1,39 @@
+//
+//  GFTextField.swift
+//  GHfollowers
+//
+//  Created by Yazan Ghunaim on 1/29/24.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true // if someone has a huge username it shrinks and adjusts
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+}
