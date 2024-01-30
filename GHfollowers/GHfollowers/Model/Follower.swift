5f0bfe0c3e491fa16795ebf34577831e4d89244f
Yazan Ghunaim <yazanghunaim07@gmail.com>
2024-01-30 21:26:38 +0100

    User | Follower models

---
+++ GHfollowers/GHfollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHfollowers
+//
+//  Created by Yazan Ghunaim on 1/30/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHfollowers/GHfollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GHfollowers
+//
+//  Created by Yazan Ghunaim on 1/30/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
