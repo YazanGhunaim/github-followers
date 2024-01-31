3f7653727206976fac4b8f505c765ed985788cfa
Yazan Ghunaim <yazanghunaim07@gmail.com>
2024-01-31 10:12:50 +0100

    refactoring error messages into an Enum

---
+++ GHfollowers/GHfollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHfollowers
+//
+//  Created by Yazan Ghunaim on 1/31/24.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse = "Invalid response from the server. Please try again later."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
